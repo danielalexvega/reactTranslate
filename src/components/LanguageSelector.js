aeea8cf0feb80e92c3cfe6d4f3526992281a5cad
Daniel Vega <danielalexvega@gmail.com>
2020-10-30 12:44:31 -0500

    write LanguageStore

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class LanguageSelector extends React.Component {
+  static contextType = LanguageContext;
   render() {
     return (
       <div>
         Select a language:
-        <i className='flag us' onClick={() => this.onLanguageChange('english')} />
-        <i className='flag nl' onClick={() => this.onLanguageChange('dutch')} />
+        <i className='flag us' onClick={() => this.context.onLanguageChange('english')} />
+        <i className='flag nl' onClick={() => this.context.onLanguageChange('dutch')} />
       </div>
     )
   }
